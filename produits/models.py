1d6548690bd090e6dc29d2188e6b29ac5ea15511
denizdurbin <denizalidurbin@gmail.com>
2025-06-21 13:18:22 +0200

    admin form, validator pour prix negatives

--- produits/models.py
+++ produits/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Produit(models.Model):
     nom = models.CharField(max_length=100)
-    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    prix = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0)])
     date_peremption = models.DateField()
 
     def __str__(self):
