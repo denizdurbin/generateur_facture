64b3ff907dab8376b27589a9893981c73d4a2a7b
denizdurbin <denizalidurbin@gmail.com>
2025-06-21 13:05:42 +0200

    Creation projet, modelisation facture/produit

---
+++ produits/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Produit(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    date_peremption = models.DateField()
+
+    def __str__(self):
+        return self.nom
+
+class Facture(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    produits = models.ManyToManyField(Produit)
+
+    def total(self):
+        return sum(p.prix for p in self.produits.all())
+
+    def __str__(self):
+        return f"Facture {self.id} du {self.date.strftime('%d/%m/%Y')}"
